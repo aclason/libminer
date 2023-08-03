00d443310f6fb2e3735bd927a5103bba7d673c7d
Alana Clason <ajclason@gmail.com>
2023-08-03 15:22:11 -0700

    import purrr::map_dbl

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -5,27 +5,33 @@
 #' @return a data.frame containing the count of pacakges in each of
 #'    your libraries
 #' @export
-#'
+#' @param sizes a logical indicating whether or not to calculate sizes
 #'
 #'
 #' @examples
 #' \dontrun{
 #' lib_summary()
+#'lib_summary(sizes = TRUE)
 #' }
 lib_summary <- function(sizes = FALSE){
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("library","n_packages")
+ # if (sizes) {
+  #  pkg_df$lib_size <- vapply(
+   #   pkg_df$library,
+    #  function(x) {
+     #   sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+    #  },
+     # FUN.VALUE = double(1)
+  #  )
+  #}
   if (sizes) {
-    pkg_df$lib_size <- vapply(
-      pkg_df$library,
-      function(x) {
-        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-      },
-      FUN.VALUE = double(1)
-    )
-
+      pkg_df$lib_size <- map_dbl(
+        pkg_df$library,
+          ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+      )
   }
 
   pkg_df
--- R/libminer-package.R
+++ R/libminer-package.R
@@ -2,5 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom purrr map_dbl
+#' @import data.table
 ## usethis namespace: end
 NULL
